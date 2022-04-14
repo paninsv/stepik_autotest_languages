b5750b3dac5912a9ae6f7dfab394a001b3354d75
s.panin <s.panin1976@mail.ru>
2022-04-14 17:00:06 +0200

    edit test_add_to_basket_button_exists

--- test_items.py
+++ test_items.py
@@ -1,7 +1,12 @@
+import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_to_basket_button_exists(browser):
     browser.implicitly_wait(10)
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+
+    time.sleep(10)  # ждем 10 сек чтобы увидеть смену языка
+    buttons = browser.find_elements_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(buttons) > 0

b0faf3546fedf085a3750e0fa81db0ed8f8fbc41
s.panin <s.panin1976@mail.ru>
2022-04-14 17:00:06 +0200

    edit test_add_to_basket_button_exists

--- conftest.py
+++ conftest.py
@@ -21,19 +21,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
-
-
-
-
-
-
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
-#

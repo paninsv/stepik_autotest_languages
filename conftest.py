8471a96aee8c02af38a0a8aa9c2468c94181873f
s.panin <s.panin1976@mail.ru>
2022-04-14 16:35:05 +0200

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--user_language', action='store', default="en",
+                     help="Choose Chrome language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("user_language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+
+
+
+
+
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
+#
